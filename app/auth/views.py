0f6bbe7df18dde20dd10c6fa0d8b1bc8d3d43db3
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-17 08:03:21 +0300

    Create Registration Form, Login Form and Logout Views

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,39 +1,46 @@
-from flask import render_template,flash, request, redirect, url_for
-from flask_login import login_user, logout_user,login_required
-from app import db
-from app.auth import auth
-from app.models import User
-from .forms import RegForm,LoginForm
-from ..email import mail_message
+from . import auth
+from flask import render_template, redirect, url_for, request
+from ..models import User
+from .forms import LoginForm, RegistrationForm
+from .. import db
+from flask_login import login_user, logout_user, login_required
 
-#View Login Page
-@auth.route('/login',methods = ['POST','GET'])
-def login():
-    form = LoginForm()
+#View Registration Form
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user != None and user.verify_password(form.password.data):
-            login_user(user, form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Wrong Username or Password')
-    return render_template('auth/login.html',form = form)
-
-#View Signup Page
-@auth.route('/signup',methods = ["POST","GET"])
-def signup():
-    form = RegForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
-        user.save()
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
         db.session.add(user)
         db.session.commit()
-        mail_message("Welcome to Legal Update!", "email/welcome", user.email, user=user)
-        return redirect(url_for('auth.login'))
-    return render_template('auth/signup.html', registration_form=form)
 
-#User Logout
+        return redirect(url_for('auth.lawyer_login'))
+        title = "New Account"
+    return render_template('auth/lawyer_register.html', registration_form=form)
+
+
+#View Login Form
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Legalupdate login"
+    return render_template('auth/lawyer_login.html', login_form=login_form, title=title)
+
+
+#View Logout
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
+    title = "Legalupdate logout"
+    return render_template('auth/lawyer_login.html', title=title)
