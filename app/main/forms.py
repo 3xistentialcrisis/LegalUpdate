8450a952a54b630a4482d9f88e1b650c23f59562
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-17 09:00:07 +0300

    Amend User Class to add role_id

--- app/main/forms.py
+++ app/main/forms.py
@@ -5,13 +5,15 @@ from wtforms.validators import Required,Email
 from flask_login import current_user
 from ..models import User
 
-class CreateStatus(FlaskForm):
-    title = StringField('Enter status title',validators=[Required()])
-    content = TextAreaField('Blog Content',validators=[Required()])
-    submit = SubmitField('Post')
-
 class CreateFile(FlaskForm):
     client_name = StringField('Enter the Client Name', validators=[Required()])
     file_name = StringField('Enter the File Name', validators=[Required()])
     file_type = StringField('Enter the File Type', validators=[Required()])
     submit = SubmitField('Save')
+
+class CreateStatus(FlaskForm):
+    title = StringField('Enter status title',validators=[Required()])
+    content = TextAreaField('Blog Content',validators=[Required()])
+    submit = SubmitField('Post')
+
+
--- app/models.py
+++ app/models.py
@@ -7,8 +7,72 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(user_id)
 
-#Add Class User and Class Role
+#Roles
+class Role(db.Model):
+    __tablename__ = 'roles'
 
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+#Users
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    bio = db.Column(db.String(255),default ='My default Bio')
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    hashed_password = db.Column(db.String(255), nullable=False)
+
+    @property
+    def set_password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @set_password.setter
+    def password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return "User: %s" % str(self.username)
+
+
+#Save Lawyers
+class Lawyers(db.Model):
+    __tablename__='lawyers'
+
+    id = db.Column(db.Integer,primary_key=True)
+    lawyer_name = db.Column(db.String(255), unique=True, nullable=False)
+    lawyer_email = db.Column(db.String(255),unique=True,index=True)
+    department = db.Column(db.String(255), unique=True, nullable=False)
+
+    def save_subscriber(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Lawyers {self.lawyer_name}'
 
 #Save Client's Files
 class Files(db.Model):
@@ -37,14 +101,13 @@ class Files(db.Model):
 
 #Save Client's Status
 class Status(db.Model):
-__tablename = 'status'
+    __tablename = 'status'
     id = db.Column(db.Integer, primary_key=True)
-    client_name = db.Column(db.String(255), unique=True, nullable=False)
-    file_name = db.Column(db.String(255), unique=True, nullable=False)
+    client_name = db.relationship('Client Name', backref='Client', lazy='dynamic')
+    file_name = db.relationship('Client File', backref='Files', lazy='dynamic')
     title = db.Column(db.String(255), unique=True, nullable=False)
     content = db.Column(db.String(255), unique=True, nullable=False)
     date_created = db.Column(db.String(255), unique=True, nullable=False)
-
 
     def save(self):
         db.session.add(self)
@@ -60,7 +123,6 @@ __tablename = 'status'
         status = Status.query.all(id=id)
 
         return status
-
 
     def __repr__(self):
         return f'Status {self.file_name}'
