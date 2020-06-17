55206bd965fc4ba4469cd7fe1fc67db7b6b3cd68
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-17 14:31:30 +0300

    Change reference to User Model to Lawyers Model

--- app/auth/views.py
+++ app/auth/views.py
@@ -26,9 +26,9 @@ def register():
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        user = User.query.filter_by(email=login_form.email.data).first()
-        if user is not None and user.verify_password(login_form.password.data):
-            login_user(user, login_form.remember.data)
+        lawyer = Lawyers.query.filter_by(email=login_form.email.data).first()
+        if lawyer is not None and Lawyers.verify_password(login_form.password.data):
+            login_user(lawyer, login_form.remember.data)
             return redirect(request.args.get('next') or url_for('main.index'))
 
         flash('Invalid username or Password')
