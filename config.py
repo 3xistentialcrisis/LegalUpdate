5d21e0d9bf6eea526d786c72e2da6f4d34b8cad1
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-17 23:33:02 +0300

    Add Mail variables under Config Class

--- config.py
+++ config.py
@@ -7,7 +7,13 @@ load_dotenv()
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
