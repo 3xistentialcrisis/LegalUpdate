3bf22e226d1afcfefcbbb05fc6a3a93063add289
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-17 15:23:47 +0300

    Add Deleate Status route

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,redirect,url_for,abort,request,flash
 from app.main import main
-from app.models import Files, Status
+from app.models import Files, Status, Lawyers
 from .forms import CreateFile, CreateStatus
 from .. import db
 from flask_login import login_required,current_user
@@ -32,11 +32,11 @@ def new_file():
 
         for client in Clients:
             return redirect(url_for('main.index'))
-        flash('You have created a new file')
+        flash('You have created a New File')
     return render_template('newfile.html', form = form)
 
 
-#Create New Client File Status
+#Create New File Status
 @main.route('/new_post', methods=['POST','GET'])
 @login_required
 def new_status():
@@ -51,8 +51,19 @@ def new_status():
         new_status.save()
 
         for client in Clients:
-            mail_message("New File Status Update", "email/new_status", client.email, new_status=new_status)
+            mail_message("A New Status Update has been made on your File", "email/new_status", client.email, new_status=new_status)
         return redirect(url_for('main.index'))
         flash('You Posted a new Status', danger)
 
     return render_template('newstatus.html', form = form)
+
+#Delete Status
+@main.route('/status/<status_id>/delete', methods = ['POST'])
+@login_required
+def delete_status(status_id):
+    status = Status.query.get(status_id)
+    if Lawyers.user != current_user:
+        abort(403)
+    status.delete()
+    flash("You have Successfully deleted the status!")
+    return redirect(url_for('lawyersdashboard.html'))
